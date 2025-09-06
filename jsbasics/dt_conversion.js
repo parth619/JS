a711af2b1b86372adead49f8a53d79db8ea24f80
parth619 <157514519+parth619@users.noreply.github.com>
2025-09-06 07:06:59 +0000

    data type conversion examples to

---
+++ jsbasics/dt_conversion.js
@@ -0,0 +1,14 @@
+//DATATYPES CONVEERSION
+
+/*let score = "32"
+console.log(typeof score);
+console.log(typeof(score));
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+// "43adf" cannot be converted into number and gives output NaN and if score = null then output (typeof) will be object
+*/
+let l = "parth"
+let Booleanisl = Boolean(l)
+console.log(typeof Booleanisl)
+console.log(Booleanisl)
